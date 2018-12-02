98ac649c612add79892b863d14215b46a6b05931
Jiri Hysek <jiri.hysek@gmail.com>
2018-12-02 01:11:03 +0100

    Fly like a bird...

--- Components/Player/Player.gd
+++ Components/Player/Player.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 export var GRAVITY = 40 * 70 #40 * 60
 export var SPEED   = 30000
 export var JUMP_SPEED  = -850
+export var FLY_FORCE   = -800
+export var FLY_SPEED   = 10000
 export var WALL_GRAVITY_THROTLING = 0.05
 export var controlled = false
 export var can_fly = false
@@ -17,6 +19,7 @@ onready var sprite = $Sprite
 onready var game = get_node("/root/Game")
 onready var progress = $ProgressBar
 
+var patrolling = false
 var dead = false
 var motion = Vector2(0,0)
 var posessing = false
@@ -25,8 +28,18 @@ var posessable_enemy
 var after_posession_timeout = 0
 var killable = true
 
+var patrol_route = []
+var next_patrol_point_idx = 0
+
 func _ready():
 	add_to_group("Killable")
+	print("in ready")
+	if false and has_node("Patrolling"):
+		patrolling = true
+		print("getting children")
+		for point in $Patrolling.get_children():
+			patrol_route.append(point.global_position)
+		print("GOT PATH: " + str(patrol_route))
 	set_physics_process(true)
 
 func start_posessing():
@@ -43,8 +56,31 @@ func stop_posessing():
 	$PosessArea.monitoring = false
 	progress.hide()
 	anim.play('StopPosessing')
-	
 
+func patrolling_process(delta):
+	motion = Vector2(0,0)
+	
+	if position.distance_to(patrol_route[next_patrol_point_idx]) < 10:
+		next_patrol_point_idx = (next_patrol_point_idx + 1) % patrol_route.size()
+	else:
+		position.x = lerp(position.x, patrol_route[next_patrol_point_idx].x, FLY_SPEED * delta)
+		position.y = lerp(position.y, patrol_route[next_patrol_point_idx].y, FLY_SPEED * delta)
+
+func controlled_flying_process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		motion.y = FLY_FORCE
+		anim.play("Flap")
+		
+	if Input.is_action_pressed('ui_right'):
+		motion.x = min(motion.x + SPEED * delta, SPEED * delta)
+		sprite.scale.x = -1
+		
+	if Input.is_action_pressed('ui_left'):
+		motion.x = max(motion.x - SPEED * delta, -SPEED * delta)
+		sprite.scale.x = 1
+	elif !Input.is_action_pressed('ui_right'):
+		motion.x = lerp(motion.x, 0, 4 * delta)
+	
 func controlled_process(delta):
 	var in_air = !ray_left.is_colliding() and !ray_right.is_colliding()
 	
@@ -94,8 +130,11 @@ func _physics_process(delta):
 		if after_posession_timeout > 0:
 			after_posession_timeout -= delta
 			return
-			
-		controlled_process(delta)
+		
+		if can_fly:
+			controlled_flying_process(delta)
+		else:
+			controlled_process(delta)
 		
 		if posessing and posess_timeout >= 0:
 			if posessable_enemy:
@@ -106,9 +145,12 @@ func _physics_process(delta):
 					posess_enemy()
 			else:
 				stop_posessing()
-	
+	elif !dead and patrolling:
+		patrolling_process(delta)
+		
 	if dead:
 		motion.x = lerp(motion.x, 0, 4 * delta)
+		
 	motion = move_and_slide(motion)
 
 func die():
@@ -119,7 +161,7 @@ func die():
 	$Blood.emitting = true
 	var cam = $Camera2D
 
-	if !posessable_enemy:
+	if !posessable_enemy and cam:
 		remove_child(cam)
 		game.add_child(cam)
 		cam.position = position
